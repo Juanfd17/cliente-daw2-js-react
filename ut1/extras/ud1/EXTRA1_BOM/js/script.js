f7cb8d763d347f10ebfd3ac23ccd0dd10b5a7be9
Juan Feito <juanfd17@educastur.es>
2023-09-27 08:28:52 +0200

    extra ud1 Extra1_bom

---
+++ ut1/extras/ud1/EXTRA1_BOM/js/script.js
@@ -0,0 +1,9 @@
+let navegador = navigator;
+
+document.write("<p>Nombre: " + navegador.appCodeName + "</p>");
+document.write("<p>Nombre  Oficial: " + navegador.appName + "</p>");
+document.write("<p>Version: " + navegador.appVersion + "</p>");
+document.write("<p>Cookies?: " + navegador.cookieEnabled + "</p>");
+document.write("<p>Plataforma: " + navegador.platform + "</p>");
+document.write("<p>Vendedor: " + navegador.vendor + "</p>");
+document.write("<p>Lenguage: " + navegador.language + "</p>");
