91460dda9159f2d9bff1256d87c19d29bca9330e
juanf <juanfd17@educastur.es>
2023-11-22 10:06:20 +0100

    22/11/23 clase ut3 propuestoFormulario.html parte

--- ut3/propuestos/Parte2_propuesto_material (formulario)/js/js.js
+++ ut3/propuestos/Parte2_propuesto_material (formulario)/js/js.js
@@ -1,5 +1,12 @@
 window.onload = princpal;
 
+const CODIGOS_ERROR={
+    NOMBRE_VACIO:1,
+    PASSWORD_CORTO:2,
+    PASSWORDS_DISTINTOS:3,
+    EMAIL_TIPO:4
+};
+
 function princpal() {
     let inputs = document.querySelectorAll('input[type = "text"]');
 
@@ -7,12 +14,24 @@ function princpal() {
         input.addEventListener("blur", salidaInput);
     }
 
+    document.querySelector("#nombre").addEventListener("blur", salidaNombre);
+
     let password2 = document.querySelector("#password2");
     password2.addEventListener("blur", contraseniasCoinciden)
 
     document.querySelector("#enviar").addEventListener("click", mostrarTodo);
 }
 
+function salidaNombre(ev) {
+    this.setCustomValidity("");
+    if (!this.validity.valid) {
+        if (this.validity.valueMissing) {
+            trataError(CODIGOS_ERROR.NOMBRE_VACIO, this.id);
+        } else {
+            limpiaError(this.id);
+        }
+    }
+}
 function salidaInput(ev) {
     ev.target.value = ev.target.value.toUpperCase();
 }
@@ -35,6 +54,14 @@ function mostrarTodo(){
     document.querySelectorAll("input[type='text']").forEach((option)=>{
         console.log(option.id + ": " + option.value);
     });
+    // type="email"
+    document.querySelectorAll("input[type='email']").forEach((option)=>{
+        console.log(option.id + ": " + option.value);
+    });
+    // type="date"
+    document.querySelectorAll("input[type='date']").forEach((option)=>{
+        console.log(option.id + ": " + option.value);
+    });
     // type="password"
     document.querySelectorAll("input[type='password']").forEach((option)=>{
         console.log(option.id + ": " + option.value);
@@ -55,4 +82,32 @@ function mostrarTodo(){
     // textarea
     let comentario = document.querySelector("#comentario");
     console.log(comentario.id + ": " + comentario.value);
+}
+
+function trataError(error, donde) {
+    var campoError=document.querySelector("#error_"+donde)
+    campoError.style.display = "block"
+    switch (error) {
+        case 1:
+            campoError.innerText = "No seas timid@, dinos tu nombre"
+        break
+
+        case 2:
+            campoError.innerText = "La contraseña es demasiado corta"
+        break
+
+        case 3:
+            campoError.innerText = "Las contraseñas no coinciden"
+        break
+
+        case 4:
+            campoError.innerText = "El correo no es valido"
+        break
+    }
+}
+
+function limpiaError(id) {
+    var campoError=document.querySelector("#error_"+id)
+    campoError.style.display = "none"
+    campoError.innerHTML = ""
 }
